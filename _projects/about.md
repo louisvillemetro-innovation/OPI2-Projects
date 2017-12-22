aec02f674afbaf2293a263a5c92ad06b8d17eb68
Matthew Gotth-Olsen <Matthew.Gotth-Olsen@louisvilleky.gov>
2017-12-22 10:38:50 -0500

    adding template

---
+++ _projects/about.md
@@ -0,0 +1,6 @@
+---
+title: About
+position: 1
+layout: redirect
+redirect_to: "/projects/_template/about/overview"
+---
--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -16,59 +16,59 @@ function togglePressed(element) {
   }
 }
 
-var parseMediumJSON = function(post_rows) {
-  // max 10 posts
-  post_rows = post_rows || 10;
-
-  // get the template
-  var template_markup = $('#post-template').html();
-
-  // get the data
-  $.getJSON('https://8k431k2y7h.execute-api.us-west-2.amazonaws.com/Production/medium', function(data) {
-    var author_list = data.body.payload.references.User;
-    var all_posts = data.body.payload.posts;
-    var posts_to_output = $.map(all_posts, function(post, index){
-      var author = author_list[post.creatorId];
-      var cleaned_post = {
-
-        post_title:       post.title,
-
-                          // get full URL, prepend Medium collection
-        post_url:         "https://medium.com/civiqueso/" + post.uniqueSlug,
-
-                          // get full Image URL, append Medium CDN
-        post_image_url:   "https://cdn-images-1.medium.com/max/1000/" + post.virtuals.previewImage.imageId,
-
-                          // get author name by ID, from user_list
-        post_author_name: author.name,
-
-                          // get author pic by ID
-        post_author_pic:  "https://cdn-images-1.medium.com/fit/c/60/60/" + author.imageId,
-
-                          // convert date to human-readable string
-        post_date:        new Date(post.latestPublishedAt).format('F j, Y')
-
-      };
-
-      // for each post, until we hit max
-      // fill out the variables
-      if (index < post_rows ) {
-        var thisInstance = $(template_markup);
-          thisInstance.find('.tile-title').text(cleaned_post.post_title);
-          thisInstance.find('.tile-link').attr('href', cleaned_post.post_url);
-          thisInstance.find('.tile-img').css('background-image', 'url(' + cleaned_post.post_image_url +')');
-          thisInstance.find('.tile-author-pic').attr('src', cleaned_post.post_author_pic).attr('alt', cleaned_post.post_author_name);
-          thisInstance.find('.tile-author').text(cleaned_post.post_author_name);
-          thisInstance.find('.tile-date').text(cleaned_post.post_date);
-        $('#post-container').append(thisInstance);
-        // and append to the container
-      }
-
-      return cleaned_post;
-    });
-
-  });
-}
+// var parseMediumJSON = function(post_rows) {
+//   // max 10 posts
+//   post_rows = post_rows || 10;
+//
+//   // get the template
+//   var template_markup = $('#post-template').html();
+//
+//   // get the data
+//   $.getJSON('https://8k431k2y7h.execute-api.us-west-2.amazonaws.com/Production/medium', function(data) {
+//     var author_list = data.body.payload.references.User;
+//     var all_posts = data.body.payload.posts;
+//     var posts_to_output = $.map(all_posts, function(post, index){
+//       var author = author_list[post.creatorId];
+//       var cleaned_post = {
+//
+//         post_title:       post.title,
+//
+//                           // get full URL, prepend Medium collection
+//         post_url:         "https://medium.com/civiqueso/" + post.uniqueSlug,
+//
+//                           // get full Image URL, append Medium CDN
+//         post_image_url:   "https://cdn-images-1.medium.com/max/1000/" + post.virtuals.previewImage.imageId,
+//
+//                           // get author name by ID, from user_list
+//         post_author_name: author.name,
+//
+//                           // get author pic by ID
+//         post_author_pic:  "https://cdn-images-1.medium.com/fit/c/60/60/" + author.imageId,
+//
+//                           // convert date to human-readable string
+//         post_date:        new Date(post.latestPublishedAt).format('F j, Y')
+//
+//       };
+//
+//       // for each post, until we hit max
+//       // fill out the variables
+//       if (index < post_rows ) {
+//         var thisInstance = $(template_markup);
+//           thisInstance.find('.tile-title').text(cleaned_post.post_title);
+//           thisInstance.find('.tile-link').attr('href', cleaned_post.post_url);
+//           thisInstance.find('.tile-img').css('background-image', 'url(' + cleaned_post.post_image_url +')');
+//           thisInstance.find('.tile-author-pic').attr('src', cleaned_post.post_author_pic).attr('alt', cleaned_post.post_author_name);
+//           thisInstance.find('.tile-author').text(cleaned_post.post_author_name);
+//           thisInstance.find('.tile-date').text(cleaned_post.post_date);
+//         $('#post-container').append(thisInstance);
+//         // and append to the container
+//       }
+//
+//       return cleaned_post;
+//     });
+//
+//   });
+// }
 
 var initMobileMenus = function() {
 
